1413f78f689a1111e726182df533cd40cf0074cd
duridev <duridev@outlook.com>
2026-02-15 13:05:27 -0300

    Update settings and URLs for principal app; add blog CSS and RTL styles; create index template

--- principal/urls.py
+++ principal/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('principal/', include('principal.urls')),
 ]
